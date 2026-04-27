86d818161dc8d1a201b61a8bb11dab9436251704
itzrobeast <robizzell@gmail.com>
2026-04-26 23:10:11 -0700

    Create email.js

---
+++ services/email.js
@@ -0,0 +1,18 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
+export async function sendEmail(to, subject, text) {
+  await transporter.sendMail({
+    from: `"Resonova" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    text
+  });
+}
