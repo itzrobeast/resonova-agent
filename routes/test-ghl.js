f584305ee00ebec0071324ad09305bc01b3702c4
itzrobeast <robizzell@gmail.com>
2026-05-01 14:45:39 -0700

    Update test-ghl.js

--- routes/test-ghl.js
+++ routes/test-ghl.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get("/test-ghl", async (req, res) => {
   try {
     const locationId = "6g4YmN9rwP8Q9Qk5njSW";
-    const apiKey = "HARDCODE_ME";
+    const apiKey = "pit-9df35977-7326-4fbc-8ac2-94d564c31a36";
 
     const response = await fetch(`https://services.leadconnectorhq.com/contacts/`, {
       method: "POST",
